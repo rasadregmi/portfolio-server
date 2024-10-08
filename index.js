26e896c003f443a1b665135771551498ce315c1f
Rasad Regmi <regmirasad53@gmail.com>
2024-10-08 20:25:22 +0545

    / removed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["https://portfolio-client-liard-three.vercel.app/"],
+  origin: ["https://portfolio-client-liard-three.vercel.app"],
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
