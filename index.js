6e3d86393802416f74a8703ca453d79110bd2418
Rasad Regmi <regmirasad53@gmail.com>
2024-10-08 20:19:15 +0545

    file changed

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://portfolio-client-liard-three.vercel.app/"],
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 
 // Routes
 app.use('/api/contact', contactRoutes);
