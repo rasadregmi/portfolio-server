f83397bb2ef255f876a3723dd7dc1e59dab882f8
Rasad Regmi <regmirasad53@gmail.com>
2024-10-09 17:31:31 +0545

    New 2

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const corsOptions = {
   credentials: true  // Allow credentials (cookies, etc.) if needed
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Preflight OPTIONS request handler
-app.options('*', cors());  // Handle preflight requests globally
+app.options('*', cors(corsOptions));  // Handle preflight requests globally
 
 // Routes
 app.use('/api/contact', contactRoutes);
